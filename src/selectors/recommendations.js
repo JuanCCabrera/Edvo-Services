4978f0a98a5fc9a5febe879a05463a16f719b2f0
Juan <juan.cabrera2@upr.edu>
2018-11-09 18:24:15 -0400

    Fixed bug in recommendations filter selector (had accidentally deleted category filtering algorithm).

--- src/selectors/recommendations.js
+++ src/selectors/recommendations.js
@@ -8,7 +8,17 @@ const getVisibleRecommendations = (recommendations, {text, category}) => {
         const schoolTypeMatch = reco.schoolType.toLowerCase().includes(text.toLowerCase());
         let typeCheck = true;
         if(category !== 'all'){
-            
+            if(category === 'Technology Integration' && !reco.technologyIntegration){
+                typeCheck = false;
+            }else if(category === 'Updated Material' && !reco.updatedMaterial){
+                typeCheck = false;
+            }else if(category === 'Teaching Strategies' && !reco.teachingStrategies){
+                typeCheck = false;
+            }else if(category === 'Instructional Alignment' && !reco.instructionAlignment){
+                typeCheck = false;
+            }else if(category === 'Time Management' && !reco.timeManagement){
+                typeCheck = false;
+            }
         }
         return (titleMatch || headerMatch || descriptionMatch || locationMatch || subjectMatch || schoolTypeMatch) && typeCheck;
     });
