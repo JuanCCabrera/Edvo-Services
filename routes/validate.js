a0e62223d462d034b304c65d73800ec09f81fec5
Tamara Gonzalez <tamara.gonzalez4@upr.edu>
2018-11-20 09:25:54 -0400

    changes to validate

--- routes/validate.js
+++ routes/validate.js
@@ -3,7 +3,7 @@ function validateNoSpace( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[a-zA-ZÀ-ÿ]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
         return true;
       }else{
         return !patt.test(str);
@@ -14,7 +14,7 @@ function validateStrings( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[a-zA-ZÀ-ÿ\s]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
         return true;
     }else{
         return !patt.test(str);
@@ -26,7 +26,7 @@ function validateStrings( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[a-zA-ZÀ-ÿ\s\,\.]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
         return true;
     }else{
         return !patt.test(str);
@@ -38,7 +38,7 @@ function validateStrings( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[0-9]{1}$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
         return true;
     }else{
         return !patt.test(str);
@@ -50,7 +50,7 @@ function validateStrings( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[0-9]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
         return true;
     }else{
         return !patt.test(str);
@@ -62,7 +62,7 @@ function validateStrings( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[a-zA-Z0-9\_]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
       return true;
     }else{
       return !patt.test(str);
@@ -74,7 +74,7 @@ function validateUserID( toValidate ) {
     var str = toValidate.toString();
     var patt = new RegExp(/^[a-zA-Z0-9\|]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
       return true;
     }else{
       return !patt.test(str);
@@ -86,7 +86,7 @@ function validateBool( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^(true|false)*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
         return true;
       }else{
         return !patt.test(str);
@@ -98,7 +98,7 @@ function validateEd( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[a-zA-Z\'\s]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
       return true;
     }else{
       return !patt.test(str);
@@ -109,29 +109,30 @@ function validateEmail( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/[a-zA-Z0-9._%+-]@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
       return true;
     }else{
       return !patt.test(str);
     }
 }
-  //validate Date
+//validate Date
 function validateDate( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
       return true;
     }else{
       return !patt.test(str);
     }
   }
+
 //validate timestamp
 function validateTime( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[0-9]{4}-[0-9]{2}-[0-9]{2}[\s]{1}[0-9]{2}\:[0-9]{2}:[0-9]{2}$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
         return true;
     }else{
         return !patt.test(str);
@@ -143,7 +144,7 @@ function validateLevel( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[a-zA-Z0-9\-\/\s]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
       return true;
     }else{
       return !patt.test(str);
@@ -155,7 +156,7 @@ function validateGroup( toValidate ) {
     var str = toValidate;
     var patt = new RegExp(/^[0-9\-\+\s]*$/);
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
       return true;
     }else{
       return !patt.test(str);
@@ -165,7 +166,7 @@ function validateGroup( toValidate ) {
 function validateLongText( toValidate){
     var str = toValidate;
     var patt1= new RegExp(/^\s/);
-    if (str == "" || patt1.test(str) ){
+    if (str === "" || patt1.test(str) ){
       return true;
     }else{
       return false;
