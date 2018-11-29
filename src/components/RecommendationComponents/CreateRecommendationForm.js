dc66b69df0da5cc3646add77e05595634aeddbe1
Juan <juan.cabrera2@upr.edu>
2018-11-29 00:42:44 -0400

    Table styles.

--- src/components/RecommendationComponents/CreateRecommendationForm.js
+++ src/components/RecommendationComponents/CreateRecommendationForm.js
@@ -435,7 +435,7 @@ class CreateRecommendationForm extends React.Component{
         if (!this.state.title || !this.state.header || !this.state.editorState || !(this.props.isEdit || this.state.question) || !this.state.subject || !this.state.topics[0] || (!this.props.isEdit && (!this.state.choices[0] || !this.state.choices[1] || !this.state.choices[2]))){
             this.setState(() => ({creationError: true}));
             //If checkboxes are empty...
-        }else if(!(this.state.teachingStrategies || this.state.updatedMaterial || this.state.timeManagement || this.state.technologyIntegration || this.state.instructionAlignment) && !(this.state.moodle || this.state.googleClassroom || this.state.emailResource || this.state.books || this.state.socialMedia || this.state.projector || this.state.computer || this.state.tablet || this.state.stylus || this.state.internet || this.state.smartboard || this.state.smartpencil || this.state.speakers)){
+        }else if(!(this.state.teachingStrategies || this.state.updatedMaterial || this.state.timeManagement || this.state.technologyIntegration || this.state.instructionAlignment) || !(this.state.moodle || this.state.googleClassroom || this.state.emailResource || this.state.books || this.state.socialMedia || this.state.projector || this.state.computer || this.state.tablet || this.state.stylus || this.state.internet || this.state.smartboard || this.state.smartpencil || this.state.speakers)){
             this.setState(() => ({creationError: true}));
             //If a regex check fails...
         }else if(this.state.titleError || this.state.headerError || this.state.descriptionError || this.state.locationError || this.state.topicError || this.state.subjectError || this.state.questionError || this.state.choiceError){
